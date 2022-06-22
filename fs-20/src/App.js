d41ce3970a6019417e22b7a13922ee830b8f4aab
balasaravanan3502 <balasaravananvp02@gmail.com>
2022-06-22 22:43:07 +0530

    FS-20 Added

--- fs-20/src/App.js
+++ fs-20/src/App.js
@@ -1,73 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import MovieComponent from "./components/MovieComponent";
-import MovieInfoComponent from "./components/MovieInfoComponent";
 
-export const API_KEY = "d228d743";
+export const API_KEY = "API_KEY";
 
 function App() {
-  const [searchQuery, updateSearchQuery] = useState("");
-
   const [movieList, updateMovieList] = useState([]);
-  const [selectedMovie, onMovieSelect] = useState();
-
-  const [timeoutId, updateTimeoutId] = useState();
 
   const fetchData = async (searchString) => {
     const response = await Axios.get(
-      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
     );
-    updateMovieList(response.data.Search);
+    updateMovieList(response.data.results);
   };
 
-  const onTextChange = (e) => {
-    onMovieSelect("");
-    clearTimeout(timeoutId);
-    updateSearchQuery(e.target.value);
-    const timeout = setTimeout(() => fetchData(e.target.value), 500);
-    updateTimeoutId(timeout);
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="app__container">
       <div className="app__header">
-        <div className="app__name">Movie finder</div>
-
-        <div className="app__searchbox">
-          <img
-            className="app__search_icon"
-            src="/react-movie-app/search-icon.svg"
-            alt="Search"
-          />
-          <input
-            className="app__input"
-            placeholder="Search Movie"
-            value={searchQuery}
-            onChange={onTextChange}
-          />
-        </div>
+        <div className="app__name">Popular Movies </div>
       </div>
-      {selectedMovie && (
-        <MovieInfoComponent
-          selectedMovie={selectedMovie}
-          onMovieSelect={onMovieSelect}
-        />
-      )}
+
       <div className="app__movielist">
-        {movieList?.length ? (
-          movieList.map((movie, index) => (
-            <MovieComponent
-              key={index}
-              movie={movie}
-              onMovieSelect={onMovieSelect}
-            />
-          ))
-        ) : (
-          <img
-            className="app__placeholder"
-            src="/react-movie-app/movie-icon.svg"
-            alt="Placeholder"
-          />
-        )}
+        {movieList?.length
+          ? movieList.map((movie, index) => (
+              <MovieComponent key={index} movie={movie} />
+            ))
+          : null}
       </div>
     </div>
   );
--- fs-20/src/components/MovieComponent.js
+++ fs-20/src/components/MovieComponent.js
@@ -1,24 +1,31 @@
 import React from "react";
+import Axios from "axios";
+
 import "./MovieComponent.css";
 import "../App.css";
 
+const API_KEY = "API_KEY";
+
 const MovieComponent = (props) => {
-  const { Title, Year, imdbID, Type, Poster } = props.movie;
+  const { movie } = props;
+
+  const getVideo = async (searchString) => {
+    const response = await Axios.get(
+      `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${API_KEY}&language=en-US`
+    );
+    window.open(
+      `https://www.youtube.com/watch?v=${response.data.results[0].key}`
+    );
+  };
 
   return (
-    <div
-      className="movie__card_container"
-      onClick={() => {
-        props.onMovieSelect(imdbID);
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      }}
-    >
-      <img src={Poster} alt={Title} className="movie__card_img" />
-      <div className="movie__card_name">{Title}</div>
-      <div className="movie__card_info">
-        <div className="movie__card_info">Year : {Year}</div>
-        <div className="movie__card_info">Type : {Type}</div>
-      </div>
+    <div className="movie__card_container" onClick={getVideo}>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+        alt={movie.title}
+        className="movie__card_img"
+      />
+      <div className="movie__card_name">{movie.title}</div>
     </div>
   );
 };
